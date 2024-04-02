100d321f8676355b19ba2485a111b804d815b45e
iankuria668 <iankuria668@gmail.com>
2024-04-02 11:23:48 +0300

    assignment done

--- index.js
+++ index.js
@@ -12,5 +12,15 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+    return tutorials.map(tutorial => {
+    let words = tutorial.split(' ');
+    let titleCase = words.map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    ;
+    return titleCase.join(' ');
+  
+  });
+   
 }
+
